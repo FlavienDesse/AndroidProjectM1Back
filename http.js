fad044813b5089c0ee6d0fec302be1ab28324b60
MrFlavZz <flav621@gmail.com>
2020-11-16 21:01:52 +0100

    heroku

--- http.js
+++ http.js
@@ -3,7 +3,7 @@ const connectDb = require('./mongoDbConnection.js');
 
 
 connectDb().then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${process.env.PORT}!`)
+  app.listen(process.env.PORT ||3000, () => {
+    console.log(`Connect to DB ${process.env.PORT}!`)
   })
 })
