b520cd733b2610953cfa7f64284fb5322c04279c
MrFlavZz <flav621@gmail.com>
2020-11-18 19:33:12 +0100

    populate content in login

--- src/routes/api/Users/login.js
+++ src/routes/api/Users/login.js
@@ -6,62 +6,72 @@ const jwt = require("jsonwebtoken");
 module.exports = function (req, res) {
     if (req.body.username !== undefined && req.body.password !== undefined) {
 
-        User.findOne({username: req.body.username}).populate("forms").populate('content').exec(async function (err, user) {
+        User.findOne({username: req.body.username}).populate("forms").populate('.content').exec(async function (err, user) {
             if (err) {
                 res.status(500).send(err);
             } else if (user !== null) {
-                bcrypt.compare(req.body.password, user.password, async function (err, result) {
+                User.populate(user, {
+                    path: 'forms.content',
+                    model: 'Widget',
+                }, function (err, result) {
                     if (result) {
-                        let token = jwt.sign(
-                            {
-                                id: user._id,
-                                type: user.type
-                            },
-                            process.env.TOKEN_SECRET,
-                            {
-                                expiresIn: 60 * 120
-                            }
-                        );
+                        bcrypt.compare(req.body.password, user.password, async function (err, result) {
+                            if (result) {
+                                let token = jwt.sign(
+                                    {
+                                        id: user._id,
+                                        type: user.type
+                                    },
+                                    process.env.TOKEN_SECRET,
+                                    {
+                                        expiresIn: 60 * 120
+                                    }
+                                );
 
-                        let tokenParts = token.split('.');
+                                let tokenParts = token.split('.');
 
-                        let headerPayload = tokenParts[0] + "." + tokenParts[1];
+                                let headerPayload = tokenParts[0] + "." + tokenParts[1];
 
-                        let signature = tokenParts[2];
+                                let signature = tokenParts[2];
 
-                        let cookieSignatureConfig = {
-                            httpOnly: true,
-                            secure: false
-                        };
+                                let cookieSignatureConfig = {
+                                    httpOnly: true,
+                                    secure: false
+                                };
 
-                        let cookieHeaderPayloadConfig = {
-                            secure: false,
-                            maxAge: 1000 * 60 * 120
-                        };
+                                let cookieHeaderPayloadConfig = {
+                                    secure: false,
+                                    maxAge: 1000 * 60 * 120
+                                };
 
-                        // place token in 2 cookies (secure way of storing tokens, better way than localStorage)
-                        res.cookie('headerPayload', headerPayload, cookieHeaderPayloadConfig);
-                        res.cookie('signature', signature, cookieSignatureConfig);
-                        // remove password from user object
-                        user.password = undefined;
+                                // place token in 2 cookies (secure way of storing tokens, better way than localStorage)
+                                res.cookie('headerPayload', headerPayload, cookieHeaderPayloadConfig);
+                                res.cookie('signature', signature, cookieSignatureConfig);
+                                // remove password from user object
+                                user.password = undefined;
 
-                        res.send(user)
-
+                                res.send(user)
                     }
+                })
 
-                    // Incorrect credentials
-                    else {
-                        res.status(202).send({message: "Incorrect credentials"});
-                    }
-                });
-            } else {
-                res.status(202).send({message: "User not found with this ID"});
+
             }
 
-        })
+            // Incorrect credentials
+            else {
+                res.status(202).send({message: "Incorrect credentials"});
+            }
+        });
+    } else {
+        res.status(202).send({message: "User not found with this ID"});
     }
-    // missing credential(s)
-    else {
-        res.status(202).send({message: "missing username or password"});
-    }
+
+}
+)
+}
+// missing credential(s)
+else
+{
+    res.status(202).send({message: "missing username or password"});
+}
 }
