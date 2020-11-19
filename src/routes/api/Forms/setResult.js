225d1e0181f8efd0456654d1cc22ce00b70b838a
MrFlavZz <flav621@gmail.com>
2020-11-19 22:59:35 +0100

    JSON parse

--- src/routes/api/Forms/setResult.js
+++ src/routes/api/Forms/setResult.js
@@ -10,9 +10,9 @@ function onlyUnique(value, index, self) {
 
 module.exports = function (req, res) {
     let ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
-    console.log(req.body.result)
+    console.log(JSON.parse(req.body.result))
+    req.body.result = JSON.parse(req.body.result)
     console.log(req.body)
-    console.log(req.body.result.toArray())
     if (req.body._id !== undefined || req.body.result !== undefined) {
         if (ObjectId.isValid(req.body._id)) {
             Form.findById(req.body._id, async function (err, doc) {
