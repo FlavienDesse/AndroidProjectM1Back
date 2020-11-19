d04f65604428ae5d55ad7aefc2171cbed057d567
MrFlavZz <flav621@gmail.com>
2020-11-19 18:23:45 +0100

    add console.log

--- src/routes/api/Forms/setResult.js
+++ src/routes/api/Forms/setResult.js
@@ -11,6 +11,8 @@ function onlyUnique(value, index, self) {
 module.exports = function (req, res) {
     let ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
     console.log(req.body.result)
+    console.log(req.body)
+    console.log(req.body.result.result)
     if (req.body._id !== undefined || req.body.result !== undefined) {
         if (ObjectId.isValid(req.body._id)) {
             Form.findById(req.body._id, async function (err, doc) {
